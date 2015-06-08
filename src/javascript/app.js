23aab1a347c3732d9d8db121f0166898b3c96c03
Antonio Solano <jsolano91@gmail.com>
2015-06-07 21:48:49 -0600

    feat(js) added js, but it does not work. Also added navbar

---
+++ src/javascript/app.js
@@ -0,0 +1,30 @@
+function init(){
+	scrollNav();
+	navColapse();
+}
+
+// Animate Scroll
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 70
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+
+// Function to collapse nav on mobile
+function navColapse(){
+	$('.navbar-collapse a').click(function(){
+    	$(".navbar-collapse").collapse('hide');
+	});
+}
+
+module.exports = init;
